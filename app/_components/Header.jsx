f030156a56f5f664a027d95c5ece6a92bc87f168
ritikdevelop <hrithikrana216@gmail.com>
2024-11-13 23:41:31 +0530

    "Updated Header component to use useEffect and added axios dependency; removed image files from public/images directory"

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,8 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,22 +11,35 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import GlobalAPi from "../_utils/GlobalAPi";
 
 function Header() {
+
+  useEffect(()=>{
+    getCategoryList();
+  },[])
+
+  //! Category list function
+  const getCategoryList=()=>{
+    GlobalAPi.getCategory().then((resp)=>{
+      console.log("category list Response: ",resp.data.data);
+  })
+}
+
   return (
     <div className="p-5 shadow-sm flex justify-between">
       <div className="flex items-center gap-8">
         <Image src={"/images/logo.png"} alt="Logo" width={150} height={100} />
 
-        <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200">
-          <LayoutGrid className="h-5 w-5" />
-          Category
-        </h2>
-
         <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuTrigger asChild>
+            <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200 cursor-pointer">
+              <LayoutGrid className="h-5 w-5" />
+              Category
+            </h2>
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
             <DropdownMenuItem>Billing</DropdownMenuItem>
