67d1f3b29564db3f468b4b26865db9fb0a52e061
ritikdevelop <hrithikrana216@gmail.com>
2024-11-17 14:04:43 +0530

    "Update baseURL in axiosClient from 'http://192.168.246.123:1337/api' to 'http://localhost:1337/api'"

--- app/_utils/GlobalAPi.jsx
+++ app/_utils/GlobalAPi.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const axiosClient = axios.create({
-    baseURL: 'http://192.168.246.123:1337/api',
+    baseURL: 'http://localhost:1337/api',
 });
 
 const getCategory =()=>axiosClient.get('/categories?populate=*');
